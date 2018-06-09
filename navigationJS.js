d119d0def9fd947d666787fe7ac604e390bf1425
webmaster83 <38005379+webmaster83@users.noreply.github.com>
2018-06-09 18:51:54 +0200

    jQuery fonctionne pas !

--- navigationJS.js
+++ navigationJS.js
@@ -1,10 +1,27 @@
 // JavaScript Document
 /* Set the width of the side navigation to 250px */
+
+	
+		
+	
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+   document.getElementById("mySidenav").style.width = "250px";
 }
 
 /* Set the width of the side navigation to 0 */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+   document.getElementById("mySidenav").style.width = "0";
 } 
+
+/*document.getElementById("mySidenav").style.width = "250px";
+	(document).ready(function(){
+			
+function openNav() {
+   $("#mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+/*function closeNav() {
+   $("#mySidenav").style.width = "0";
+} 
+});*/
