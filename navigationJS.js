4d1af1743958bd02d8f5695dfc8079cdc2b2dd7f
webmaster83 <38005379+webmaster83@users.noreply.github.com>
2018-06-10 15:54:38 +0200

    Favicon + menu en Grand transparent Full Screen

--- navigationJS.js
+++ navigationJS.js
@@ -5,15 +5,11 @@
 		
 	
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    document.getElementById("mySidenav").style.width = "100%";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-    document.body.style.backgroundColor = "white";
 }
 
 /*document.getElementById("mySidenav").style.width = "250px";
