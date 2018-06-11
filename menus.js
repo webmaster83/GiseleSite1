bacff9fad4eb949fa78aaedcdab7e730d7619381
webmaster83 <38005379+webmaster83@users.noreply.github.com>
2018-06-11 11:25:41 +0200

    Barre de Recherche dans index et Editorial

---
+++ menus.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+/* Set the width of the side navigation to 250px */
+
+	
+		
+	
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+$(document).ready(function(){
+	
+});
+/*document.getElementById("mySidenav").style.width = "250px";
+	(document).ready(function(){
+			
+function openNav() {
+   $("#mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+/*function closeNav() {
+   $("#mySidenav").style.width = "0";
+} 
+});*/
